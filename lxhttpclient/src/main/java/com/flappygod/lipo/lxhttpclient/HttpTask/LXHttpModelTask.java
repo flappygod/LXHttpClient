da4affc2f2a889bf3dff978c68a259ce3b90286c
lijunlin <327603258@qq.com>
2018-08-16 18:33:56 +0800

    对泛型进行一些处理

--- lxhttpclient/src/main/java/com/flappygod/lipo/lxhttpclient/HttpTask/LXHttpModelTask.java
+++ lxhttpclient/src/main/java/com/flappygod/lipo/lxhttpclient/HttpTask/LXHttpModelTask.java
@@ -1,15 +1,15 @@
 package com.flappygod.lipo.lxhttpclient.HttpTask;
 
-import com.flappygod.lipo.lxhttpclient.Generic.Response;
 import com.flappygod.lipo.lxhttpclient.HttpTask.Base.LXHttpBaseTask;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 
 public class LXHttpModelTask<T>  extends LXHttpBaseTask<T> {
+
+    private Class<T> mClass;
 
     /**************
      * 构造器
@@ -17,23 +17,15 @@ public class LXHttpModelTask<T>  extends LXHttpBaseTask<T> {
      *            地址
      * @param hashMap
      */
-    public LXHttpModelTask(String url, HashMap<String, Object> hashMap) {
+    public LXHttpModelTask(String url, HashMap<String, Object> hashMap ,Class<T> mClass) {
         super(url, hashMap);
+        this.mClass=mClass;
     }
 
 
     @Override
     public T preParseData(String data) throws Exception {
-        //创建一个对象，用来获取泛型类型
-        Response<T> response=new Response<T>(){
-        };
-        //得到类型
-        Class<T> clazz = response.getTClass();
-        //使用gson
-        Gson gson=new Gson();
-        //进行转换
-        T t=gson.fromJson(data,clazz);
-        //返回数据
-        return t;
+        T rootBean = new Gson().fromJson(data,mClass);
+        return rootBean;
     }
 }
--- lxhttpclient/src/main/java/com/flappygod/lipo/lxhttpclient/HttpTask/LXHttpsModelTask.java
+++ lxhttpclient/src/main/java/com/flappygod/lipo/lxhttpclient/HttpTask/LXHttpsModelTask.java
@@ -1,6 +1,5 @@
 package com.flappygod.lipo.lxhttpclient.HttpTask;
 
-import com.flappygod.lipo.lxhttpclient.Generic.Response;
 import com.flappygod.lipo.lxhttpclient.HttpTask.Base.LXHttpsBaseTask;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -10,33 +9,23 @@ import java.util.HashMap;
 
 public class LXHttpsModelTask<T> extends LXHttpsBaseTask<T> {
 
+    private Class<T> mClass;
 
     /**************
      * 构造器
-     *  @param url     地址
-     * @param hashMap 请求参数
+     *  @param url
+     *            地址
+     * @param hashMap
      */
-    public LXHttpsModelTask(String url, HashMap<String, Object> hashMap) {
+    public LXHttpsModelTask(String url, HashMap<String, Object> hashMap ,Class<T> mClass) {
         super(url, hashMap);
+        this.mClass=mClass;
     }
 
-    /*****************
-     * 预处理解析数据
-     * @param data          数据
-     * @return
-     * @throws Exception
-     */
-    public  T  preParseData(String data) throws Exception{
-        //创建一个对象，用来获取类型
-        Response<T> response=new Response<T>(){
-        };
-        //得到类型
-        Class<T> clazz = response.getTClass();
-        //使用gson
-        Gson gson=new Gson();
-        //进行转换
-        T t=gson.fromJson(data,clazz);
-        //返回数据
-        return t;
+
+    @Override
+    public T preParseData(String data) throws Exception {
+        T rootBean = new Gson().fromJson(data,mClass);
+        return rootBean;
     }
 }
